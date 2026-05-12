1e2c9a42e5d584062d93c15ee47857b78a9053f2
Hazem <abdelazez.hazem@gmail.com>
2026-05-12 20:37:53 +0300

    Applied useState and useEffect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,53 @@
 import React from 'react'
 import { useEffect } from 'react';
+import axios from 'axios';
 
+import { useState } from 'react';
 
 
 
 
 export default function Contact() {
+
+ 
+ let [users, setUsers] = useState([])
+
+let getUsers = async () => {
+    let response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    setUsers(response.data);
+ }
+
   useEffect(()=>{
-    console.log("mounting phase");
-    return()=>{
-      console.log("unmounting phase");
-    }
-  }, [])
+  getUsers();
+},[])
+
   return (
     <>
     <div className='container-fluid text-center bg-danger p-4'>
     <h1>Contact</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi ipsa corporis sint quasi cumque dolore iste molestias. Magni debitis fugiat voluptas? Totam sunt labore laborum eos magni quos tenetur voluptatem!</p>
+    <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">User Name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    {users.map((user)=>(
+      <tr key={user.id}>
+      <th scope="row">{user.id}</th>  
+      <td>{user.name}</td>
+      <td>{user.username}</td>
+      <td>{user.email}</td>
+    </tr>
+    ))}
+  </tbody>
+</table>
     </div>
     </>
   )
 }
+
+
