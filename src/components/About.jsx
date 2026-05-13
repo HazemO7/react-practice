9b39bf4462a4fd1e3f4b95c7c8fd16f6d6f26985
Hazem <abdelazez.hazem@gmail.com>
2026-05-13 10:02:05 +0300

    dd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='container-fluid text-center bg-info p-4'>
         <h1 >About</h1>
-        <p>Lorem, ipsum dolor sit sth text fweuhge . Sequi ipsa corporis sint quasi cumque dolore iste molestias. Magni debitis fugiat voluptas? Totam sunt labore laborum eos magni quos tenetur voluptatem!</p>
+        <p>Lorem, ipsum dolor sit sth trioy fweuhge . Sequi ipsa corporis sint quasi cumque dolore iste molestias. Magni debitis fugiat voluptas? Totam sunt labore laborum eos magni quos tenetur voluptatem!</p>
     </div>
   )
 }
