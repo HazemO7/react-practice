62b7e2bf81eeb828959fe55d898f3288f8161b8f
HazemO7 <abdelazez.hazem@gmial.com>
2026-05-30 00:01:23 +0300

    new data type in typescript

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,10 +1,15 @@
 (function(){
+  // static data types
   var userName:string= "hazem";
   var userAge:number=40;
   var user:object={name:"ahmed", age:"34"};
   var onSale:boolean=true;
   var friends:string[]=["Hazem","Abdelazez","ave"];
-var numb:number[]=[23,56,67];
+  var numb:number[]=[23,56,67];
+  // new data types {any, tuple, union}
+  var myName:any="hazem";
+  var myFriends:[string,number,boolean]=["hazem",23,true];
+  var myAge:number|string=23;
 
 function sayHello(name:string){
 return(`hello ${name}`);
