70155b5822abd4b7c037ff7e95846e753411c69b
HazemO7 <abdelazez.hazem@gmial.com>
2026-05-29 19:44:19 +0300

    typescript practice

---
+++ TypeScript/main.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var userName = "hazem";
+    var userAge = 40;
+    var user = { name: "ahmed", age: "34" };
+    var onSale = true;
+    var friends = ["Hazem", "Abdelazez", "ave"];
+    var numb = [23, 56, 67];
+    function sayHello(name) {
+        return (`hello ${name}`);
+    }
+    sayHello("John");
+}());
