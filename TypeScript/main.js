0e7899f7ccc8456911f45820c0a52f0b2f6192cf
HazemO7 <abdelazez.hazem@gmial.com>
2026-05-30 00:06:57 +0300

    upload all files

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -1,11 +1,16 @@
 "use strict";
 (function () {
+    // static data types
     var userName = "hazem";
     var userAge = 40;
     var user = { name: "ahmed", age: "34" };
     var onSale = true;
     var friends = ["Hazem", "Abdelazez", "ave"];
     var numb = [23, 56, 67];
+    // new data types {any, tuple, union}
+    var myName = "hazem";
+    var myFriends = ["hazem", 23, true];
+    var myAge = 23;
     function sayHello(name) {
         return (`hello ${name}`);
     }
