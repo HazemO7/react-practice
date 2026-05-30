7f1a998ea9358a80239757d4064cc215e00cb471
HazemO7 <abdelazez.hazem@gmial.com>
2026-05-31 00:07:20 +0300

    interfaces in typeScript

---
+++ TypeScript/interfaces.ts
@@ -0,0 +1,8 @@
+export interface product{
+    id:number;
+    name:string;
+    price:number;
+    category:string;
+    onSale?:boolean;
+    sayHello?(name:string):string; 
+}
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,3 +1,4 @@
+import { product } from "./interfaces";
 (function(){
   var userName:string= "hazem";
   var userAge:number=40;
@@ -5,6 +6,12 @@
   var onSale:boolean=true;
   var friends:string[]=["Hazem","Abdelazez","ave"];
 var numb:number[]=[23,56,67];
+
+ var product1:product[]=[
+  {id:1,name:"laptop",price:5000,category:"electronics"},
+  {id:1,name:"laptop",price:5000,category:"electronics", onSale:true},
+  {id:1,name:"laptop",price:5000,category:"electronics"}
+  ];
 
 function sayHello(name:string){
 return(`hello ${name}`);
