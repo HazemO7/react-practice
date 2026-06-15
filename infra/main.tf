e91b51498780e7607d42b326885c10043dd1c79e
hazem <abdelazez.hazem@gmail.com>
2026-06-15 06:00:08 +0000

    create vpc module

--- infra/main.tf
+++ infra/main.tf
@@ -1,143 +1,30 @@
-# =========================
+
 # VPC
-# =========================
 
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+module "vpc" {
+  source = "./modules/VPC"
 
-  tags = {
-    Name = var.vpc_name
-  }
+  vpc_name             = "hazem-vpc"
+  vpc_cidr_block       = "10.0.0.0/16"
+
+  public_subnet_1_cidr = "10.0.1.0/24"
+  public_subnet_2_cidr = "10.0.2.0/24"
+
+  az_1 = "eu-west-1a"
+  az_2 = "eu-west-1b"
 }
 
-# =========================
-# Public Subnet 1
-# =========================
 
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-# =========================
-# Public Subnet 2
-# =========================
-
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-west-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-# =========================
-# Internet Gateway
-# =========================
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "hazem-igw"
-  }
-}
-
-# =========================
-# Route Table
-# =========================
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
-# Default Route
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-# =========================
-# Route Table Associations
-# =========================
-
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
-
-# =========================
-# Security Group
-# =========================
-
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "HTTP"
-
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
-}
-
+# ALB
 module "alb" {
   source = "./modules/ALB"
 
   alb_name          = "hazem-alb"
-  vpc_id            = aws_vpc.main.id
-  security_group_id = aws_security_group.web_sg.id
+  vpc_id            = module.vpc.vpc_id
+  security_group_id = module.vpc.security_group_id
 
   subnet_ids = [
-    aws_subnet.public_1.id,
-    aws_subnet.public_2.id
+    module.vpc.subnet_1_id,
+    module.vpc.subnet_2_id
   ]
 }
---
+++ infra/modules/VPC/main.tf
@@ -0,0 +1,96 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = var.az_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = var.az_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "hazem-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
